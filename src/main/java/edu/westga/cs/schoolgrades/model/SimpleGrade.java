2c7f42628c9d77ab5df6cf4e48be7d87ff81f1e2
Greg Murrell <j_greg_m@comcast.net>
2020-10-09 14:35:48 -0400

    SimpleGrade class written and documented

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
@@ -0,0 +1,46 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * 	This class simply a double value as a student's grade, while providing
+ * an implementation of the Grade interface.
+ * 
+ * @author Greg Murrell
+ * @version 10/09/2020
+ */
+public class SimpleGrade implements Grade {
+
+	private double value;
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public SimpleGrade(double value) {
+		this.setValue(value);
+	}
+	
+	/**
+	 * See: {@link Grade#getValue()}
+	 */
+	@Override
+	public double getValue() {
+		return this.value;
+	}
+	
+	/**
+	 * Sets the value of the student's grade to the specified value.
+	 * 
+	 * @param value		New value of the student's grade
+	 * 
+	 * @precondition	value > 0
+	 * 
+	 * @throws IllegalArgumentException		If value's range is invalid
+	 */
+	public void setValue(double value) {
+		if (value < 0) {
+			throw new IllegalArgumentException("Value for grade cannot be negative");
+		}
+		this.value = value;
+	}
+
+}
