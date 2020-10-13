dc633fd4b09afe6c62d9a880da99bc699ef50ce6
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 13:27:42 -0400

    ready to test

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/SimpleGradeWhenSetValue.java
@@ -0,0 +1,14 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+/**
+ * @author Greg Murrell
+ * @version 10/13/2020
+ */
+class SimpleGradeWhenSetValue {
+
+	
+
+}
