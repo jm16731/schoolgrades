557a8c2e0423812cab33bf5ab8a837a339152c51
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 14:03:22 -0400

    WeightedGrade, basic javadoc

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
@@ -0,0 +1,17 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * 	This class models a Grade after a Weight has been applied to it.
+ * 
+ * @author Greg Murrell
+ * @version 10/13/2020
+ */
+public class WeightedGrade implements Grade {
+
+	@Override
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
