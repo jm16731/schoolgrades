f3e8964af7536ba7e08ea2efbe860d6be86d0f9c
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 16:20:32 -0400

    CompositeGrade set strategy JUnit tested

--- src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenSetGradingStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenSetGradingStrategy.java
@@ -4,11 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/** 
+ * @author Greg Murrell
+ * @version 10/13/2020
+ */
 class CompositeGradeWhenSetGradingStrategy {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testSetStrategyToAverageOfGradesAfterNoArgsConstructionDoesWork() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.setGradingStrategy(new AverageOfGradesGradingStrategy());
+		assertEquals("AverageOfGradesGradingStrategy", grades.getGradingStrategy().getClass().getSimpleName());
+	}
+	
+	@Test
+	public void testSetStrategyToSumOfGradesAfterNoArgsConstructionDoesWork() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.setGradingStrategy(new SumOfGradesGradingStrategy());
+		assertEquals("SumOfGradesGradingStrategy", grades.getGradingStrategy().getClass().getSimpleName());
+	}
+	
+	@Test
+	public void testSetStrategyToNullShouldNotWork() {
+		CompositeGrade grades = new CompositeGrade();
+		try {
+			grades.setGradingStrategy(null);
+			fail("Was able to set CompositeGrade's GradingStrategy to null");
+		} catch (Exception e) {
+			
+		}
 	}
 
 }
