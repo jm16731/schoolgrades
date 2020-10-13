f67d712b5fc792fbf3af5b00dfa394506760a04b
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 15:47:28 -0400

    CompositeGrade test classes created

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenAddGrade.java
@@ -0,0 +1,14 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompositeGradeWhenAddGrade {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
