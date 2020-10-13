5ad3c0c1e87997cef2023eb623cf089e01afe38c
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 15:04:23 -0400

    sum of grades strategy created

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SumOfGradesCompositeGradingStrategy.java
@@ -0,0 +1,15 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * @author Greg
+ *
+ */
+public class SumOfGradesCompositeGradingStrategy implements CompositeGradingStrategy {
+
+	@Override
+	public double calculateGrade(CompositeGrade grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
