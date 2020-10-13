150c9b01c9435b6db1c5bbf086e4ce0f04dc3383
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 16:30:56 -0400

    addGrade JUnit tested

--- src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenAddGrade.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenAddGrade.java
@@ -1,8 +1,6 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +11,55 @@ import org.junit.jupiter.api.Test;
 class CompositeGradeWhenAddGrade {
 
 	@Test
-	public void testAddDouble95DoesPutThatIntoTheList() {
+	public void testAddDoubleOf95DoesPutThatIntoTheList() {
 		CompositeGrade grades = new CompositeGrade();
 		grades.addGrade(95);
-		ArrayList<Grade> listOfGrades = grades.getListOfGrades();
-		assertEquals(95, listOfGrades.get(0).getValue());
+		assertEquals(95, grades.getListOfGrades().get(0).getValue());
+	}
+	
+	@Test
+	public void testAddDoubleOf95AndOf75DoesPutThoseIntoTheList() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.addGrade(95);
+		grades.addGrade(75);
+		assertEquals(95, grades.getListOfGrades().get(0).getValue());
+		assertEquals(75, grades.getListOfGrades().get(1).getValue());
+	}
+	
+	@Test
+	public void testAddSimpleGradeOf95AndOf75DoesPutThoseIntoTheList() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.addGrade(new SimpleGrade(95));
+		grades.addGrade(new SimpleGrade(75));
+		assertEquals(95, grades.getListOfGrades().get(0).getValue());
+		assertEquals(75, grades.getListOfGrades().get(1).getValue());
+	}
+	
+	@Test
+	public void testAddCompositeGradeDoesPutThatIntoTheList() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.addGrade(new CompositeGrade());
+		assertEquals("CompositeGrade", grades.getListOfGrades().get(0).getClass().getSimpleName());
+	}
+	
+	@Test
+	public void testAddWeightedGradeDoesPutThatIntoTheList() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.addGrade(new WeightedGrade(new SimpleGrade(95)));
+		assertEquals("WeightedGrade", grades.getListOfGrades().get(0).getClass().getSimpleName());
+	}
+	
+	@Test
+	public void testAddDoubleSimpleCompositeWeightedGradeDoesAddThemInInThatOrder() {
+		CompositeGrade grades = new CompositeGrade();
+		grades.addGrade(95);
+		grades.addGrade(new SimpleGrade(95));
+		grades.addGrade(new CompositeGrade());
+		grades.addGrade(new WeightedGrade(new SimpleGrade(95)));
+		assertEquals(95, grades.getListOfGrades().get(0).getValue());
+		assertEquals(95, grades.getListOfGrades().get(1).getValue());
+		assertEquals("CompositeGrade", grades.getListOfGrades().get(2).getClass().getSimpleName());
+		assertEquals("WeightedGrade", grades.getListOfGrades().get(3).getClass().getSimpleName());
 	}
 
 }
