07e7e0a1034d84dc313753697be2a0a362a9918d
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 14:43:14 -0400

    ready to test

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/WeightedGradeWhenSetWeight.java
@@ -0,0 +1,15 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Greg Murrell
+ * @version 10/13/2020
+ */
+class WeightedGradeWhenSetWeight {
+
+
+
+}
