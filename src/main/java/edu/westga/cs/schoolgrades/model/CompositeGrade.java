660902424fe4e55db8072cc8d0b7553fdb5a779e
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 15:08:33 -0400

    CompositeGrade written and documented

--- src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -14,18 +14,48 @@ public class CompositeGrade implements Grade {
 	private CompositeGradingStrategy gradingStrategy;
 	
 	/**
-	 * No-args construction of instance variables
+	 * 	Initializes instance variables, sets a default CompositeGradingStrategy
+	 * as SumOfGradesCompositeGradingStrategy
 	 */
 	public CompositeGrade() {
-		this.grades = new ArrayList<Grade>();
+		this(new SumOfGradesCompositeGradingStrategy());
 	}
 	
+	/**
+	 * Construct a Composite grade according to the given CompositeGradingStrategy
+	 * 
+	 * @param gradingStrategy		The CompositeGradingStrategy for grading
+	 */
 	public CompositeGrade(CompositeGradingStrategy gradingStrategy) {
 		this.grades = new ArrayList<Grade>();
+		this.setGradingStrategy(gradingStrategy);
 	}
 	
+	/**
+	 * Sets the CompositeGradingStrategy to the given gradingStrategy
+	 * 
+	 * @param gradingStrategy		The new CompositeGradingStrategy
+	 */
 	public void setGradingStrategy(CompositeGradingStrategy gradingStrategy) {
 		this.gradingStrategy = gradingStrategy;
+	}
+	
+	/**
+	 * Gets this CompositeGrade's CompositeGradingStrategy
+	 * 
+	 * @return		This instance's CompositeGradingStrategy
+	 */
+	public CompositeGradingStrategy getGradingStrategy() {
+		return this.gradingStrategy;
+	}
+	
+	/**
+	 * Gets the list of grades maintained by this CompositeGrade
+	 * 
+	 * @return		This CompositeGrade's list of grades
+	 */
+	public ArrayList<Grade> getListOfGrades() {
+		return this.grades;
 	}
 	
 	/**
@@ -38,21 +68,11 @@ public class CompositeGrade implements Grade {
 	}
 	
 	/**
-	 * Gets the list of grades maintained by this CompositeGrade
-	 * 
-	 * @return		This CompositeGrade's list of grades
-	 */
-	public ArrayList<Grade> getListOfGrades() {
-		return this.grades;
-	}
-
-	/**
 	 * See: {@link Grade#getValue()}
 	 */
 	@Override
 	public double getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.gradingStrategy.calculateGrade(this);
 	}
 
 }
