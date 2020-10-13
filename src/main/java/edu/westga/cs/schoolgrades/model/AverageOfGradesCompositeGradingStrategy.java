1e5d238aa2c76e8929951a507c556851c1621036
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 15:37:09 -0400

    class created

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/AverageOfGradesCompositeGradingStrategy.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * @author Greg
+ *
+ */
+public class AverageOfGradesCompositeGradingStrategy implements CompositeGradingStrategy {
+
+	@Override
+	public double calculateGrade(CompositeGrade grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
