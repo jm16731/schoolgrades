7b829d8a6fe6b276ddb58f33c5769419405c256f
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 15:49:19 -0400

    GradingStrategy test classes created

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/SumOfGradesGradingStrategyWhenCalculateGrade.java
@@ -0,0 +1,14 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumOfGradesGradingStrategyWhenCalculateGrade {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
