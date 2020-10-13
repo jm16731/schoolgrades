0e3bd7ddb9517556bce775c3c340a04033b594bf
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 14:48:14 -0400

    JUnit tested

--- src/test/java/edu/westga/cs/schoolgrades/model/WeightedGradeWhenSetWeight.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/WeightedGradeWhenSetWeight.java
@@ -2,6 +2,9 @@ package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,6 +13,72 @@ import org.junit.jupiter.api.Test;
  */
 class WeightedGradeWhenSetWeight {
 
-
+	@Test
+	public void testSetWeightTo0Point35() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade);
+		weighted.setWeight(0.35);
+		
+		BigDecimal accurateDouble = new BigDecimal(weighted.getValue());
+		accurateDouble = accurateDouble.setScale(2, RoundingMode.HALF_EVEN);
+		assertEquals(33.25, accurateDouble.doubleValue());
+	}
+	
+	@Test
+	public void testSetWeightTo0Point67() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade);
+		weighted.setWeight(0.67);
+		
+		BigDecimal accurateDouble = new BigDecimal(weighted.getValue());
+		accurateDouble = accurateDouble.setScale(2, RoundingMode.HALF_EVEN);
+		assertEquals(63.65, accurateDouble.doubleValue());
+	}
+	
+	@Test
+	public void testSetWeightTo0() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade);
+		weighted.setWeight(0);
+		assertEquals(0, weighted.getValue());
+	}
+	
+	@Test
+	public void testSetWeightWhenNotInitiallyConstructedAsDefaultTo0Point35() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade, 0.01);
+		weighted.setWeight(0.35);
+		
+		BigDecimal accurateDouble = new BigDecimal(weighted.getValue());
+		accurateDouble = accurateDouble.setScale(2, RoundingMode.HALF_EVEN);
+		assertEquals(33.25, accurateDouble.doubleValue());
+	}
+	
+	@Test
+	public void testSetWeightWhenNotInitiallyConstructedAsDefaultTo0Point67() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade, 0.01);
+		weighted.setWeight(0.67);
+		
+		BigDecimal accurateDouble = new BigDecimal(weighted.getValue());
+		accurateDouble = accurateDouble.setScale(2, RoundingMode.HALF_EVEN);
+		assertEquals(63.65, accurateDouble.doubleValue());
+	}
+	
+	@Test
+	public void testSetWeightWhenNotInitiallyConstructedAsDefaultTo0() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade, 0.01);
+		weighted.setWeight(0);
+		assertEquals(0, weighted.getValue());
+	}
+	
+	@Test
+	public void testSetWeightWhenNotInitiallyConstructedAsDefaultTo1() {
+		SimpleGrade grade = new SimpleGrade(95);
+		WeightedGrade weighted = new WeightedGrade(grade, 0.01);
+		weighted.setWeight(1);
+		assertEquals(95, weighted.getValue());
+	}
 
 }
