9ea915f4cac6fdbf4e54cedeb33f09370fee003c
Greg Murrell <j_greg_m@comcast.net>
2020-10-13 16:05:10 -0400

    CompositeGrade construction JUnit tested

--- src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenConstruct.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenConstruct.java
@@ -1,14 +1,45 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Greg Murrell
+ * @version 10/13/2020
+ */
 class CompositeGradeWhenConstruct {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testConstructDefaultCompositeGradeShouldConstruct() {
+		try {
+			CompositeGrade grades = new CompositeGrade();
+		}
+		catch (Exception e) {
+			fail("CompositeGrade no-args constructor failed");
+		}
+	}
+	
+	@Test
+	public void testConstructCompositeGradeWithGivenGradingStrategyShouldConstruct() {
+		try {
+			CompositeGrade sumGrades = new CompositeGrade(new SumOfGradesGradingStrategy());
+			CompositeGrade averageGrades = new CompositeGrade(new AverageOfGradesGradingStrategy());
+		}
+		catch (Exception e) {
+			fail("CompositeGrade gradingStrategy constructor failed");
+		}
+	}
+	
+	@Test
+	public void testConstructCompositeGradeWithNullGradingStrategyShouldNotConstruct() {
+		try {
+			CompositeGrade grades = new CompositeGrade(null);
+			fail("CompositeGrade was constructed with null GradingStrategy");
+		}
+		catch (Exception e) {
+			
+		}
 	}
 
 }
