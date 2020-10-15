ff2190e2e1e1e27067389787261d889a1a494627
Greg Murrell <j_greg_m@comcast.net>
2020-10-15 15:52:14 -0400

    ready to test

--- src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenGetValue.java
@@ -1,14 +1,15 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Greg Murrell
+ * @version 10/15/2020
+ */
 class CompositeGradeWhenGetValue {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
 
 }
