bccf53763b8d38560608595b872f864662365761
Greg Murrell <j_greg_m@comcast.net>
2020-10-15 15:37:24 -0400

    doesn't drop lowest with 0 or 1 grade

--- src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeGradingStrategy.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeGradingStrategy.java
@@ -36,6 +36,9 @@ public class DropLowestGradeGradingStrategy implements GradingStrategy {
 	@Override
 	public double calculateGrade(CompositeGrade grades) {
 		ArrayList<Grade> listOfGrades = grades.getListOfGrades();
+		if (listOfGrades.size() <= 1) {
+			return this.gradingStrategyToBeDecorated.calculateGrade(grades);
+		}
 		
 		double[] values = new double[listOfGrades.size()];
 		for (int i = 0; i < listOfGrades.size(); i++) {
