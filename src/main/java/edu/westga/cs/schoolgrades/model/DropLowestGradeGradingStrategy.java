ce807e6dc8f743faf5862be50a312424a959631b
Greg Murrell <j_greg_m@comcast.net>
2020-10-15 15:20:27 -0400

    drop lowest is written and documented

--- src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeGradingStrategy.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeGradingStrategy.java
@@ -1,18 +1,52 @@
-/**
- * 
- */
 package edu.westga.cs.schoolgrades.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
- * @author Greg
- *
+ * 	This class decorates another GradingStrategy so as to drop
+ * the lowest Grade before calculating the total grade.
+ * 
+ * @author Greg Murrell
+ * @version 10/15/2020
  */
 public class DropLowestGradeGradingStrategy implements GradingStrategy {
+	private final GradingStrategy gradingStrategyToBeDecorated;
+	
+	/**
+	 * Constructs an instance with the given GradingStrategy
+	 * 
+	 * @param gradingStrategyToBeDecorated		GradingStrategy where the lowest grade is to be dropped
+	 * 
+	 * @precondition 							gradedStrategyToBeDecorated is not null
+	 * 
+	 * @throws NullPointerException				if gradingStrategyToBeDecorated is null
+	 */
+	public DropLowestGradeGradingStrategy(GradingStrategy gradingStrategyToBeDecorated) {
+		if (gradingStrategyToBeDecorated == null) {
+			throw new NullPointerException("Cannot construct DropLowestGradeGradingStrategy with "
+					+ "a null value for gradingStrategyToBeDecorated");
+		}
+		this.gradingStrategyToBeDecorated = gradingStrategyToBeDecorated;
+	}
 
+	/**
+	 * See: {@link GradingStrategy#calculateGrade()}
+	 */
 	@Override
 	public double calculateGrade(CompositeGrade grades) {
-		// TODO Auto-generated method stub
-		return 0;
+		ArrayList<Grade> listOfGrades = grades.getListOfGrades();
+		
+		double[] values = new double[listOfGrades.size()];
+		for (int i = 0; i < listOfGrades.size(); i++) {
+			values[i] = listOfGrades.get(i).getValue();
+		}
+		java.util.Arrays.sort(values);
+		double[] valuesWithoutLowest = Arrays.copyOfRange(values, 1, values.length - 1);
+		
+		CompositeGrade compositeWithoutLowest = new CompositeGrade();
+		compositeWithoutLowest.addGrade(valuesWithoutLowest);
+		return this.gradingStrategyToBeDecorated.calculateGrade(compositeWithoutLowest);
 	}
 
 }
