25127503e283fecb28db02feebf8816d26d50e4e
Greg Murrell <j_greg_m@comcast.net>
2020-10-15 15:25:52 -0400

    ready to test

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/DropLowestGradeGradingStrategyWhenConstruct.java
@@ -0,0 +1,19 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Greg Murrell
+ * @version 10/15/2020
+ */
+class DropLowestGradeGradingStrategyWhenConstruct {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
