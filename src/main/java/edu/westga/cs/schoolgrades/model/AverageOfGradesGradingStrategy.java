57fec60c6add8f9f6bb6a9d5863db81c0adf4370
Greg Murrell <j_greg_m@comcast.net>
2020-10-15 13:35:51 -0400

    average of grading strategy written, documented

--- src/main/java/edu/westga/cs/schoolgrades/model/AverageOfGradesGradingStrategy.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/AverageOfGradesGradingStrategy.java
@@ -1,18 +1,28 @@
-/**
- * 
- */
 package edu.westga.cs.schoolgrades.model;
 
+import java.util.ArrayList;
+
 /**
- * @author Greg
- *
+ * 	This class implements a GradingStrategy of taking the average
+ * of all the given grades.
+ * 
+ * @author Greg Murrell
+ * @version 10/15/2020
  */
 public class AverageOfGradesGradingStrategy implements GradingStrategy {
-
+	
+	/**
+	 * See: {@link GradingStrategy#calculateGrade()}
+	 */
 	@Override
 	public double calculateGrade(CompositeGrade grades) {
-		// TODO Auto-generated method stub
-		return 0;
+		double sum = 0;
+		int total = 0;
+		for (Grade grade : grades.getListOfGrades()) {
+			sum += grade.getValue();
+			total++;
+		}
+		return sum / total;
 	}
 
 }
