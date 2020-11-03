f0bde99fc66d677571a7c1bc9ebab5d9e9ecd765
Greg Murrell <j_greg_m@comcast.net>
2020-11-03 16:07:07 -0500

    GradeTabelModel written

--- src/main/java/edu/westga/cs/schoolgrades/views/GradeTableModel.java
+++ src/main/java/edu/westga/cs/schoolgrades/views/GradeTableModel.java
@@ -1,6 +1,11 @@
 package edu.westga.cs.schoolgrades.views;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
+
+import edu.westga.cs.schoolgrades.model.Grade;
 
 /**
  * Implements the TableModel interface, using Grades as the rows
@@ -9,11 +14,15 @@ import javax.swing.table.AbstractTableModel;
  * @version 11/03/2020
  */
 public class GradeTableModel extends AbstractTableModel {
-
+	private List<Grade> rows;
+	
+	public GradeTableModel() {
+		this.rows = new ArrayList<Grade>();
+	}
+	
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.rows.size();
 	}
 
 	@Override
@@ -22,9 +31,15 @@ public class GradeTableModel extends AbstractTableModel {
 	}
 
 	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+	public Grade getValueAt(int rowIndex, int columnIndex) {
+		if (rowIndex < 0) {
+			throw new IndexOutOfBoundsException("TableModel cannot return a getValueAt for rowIndex less than zero");
+		}
+		return this.rows.get(rowIndex);
+	}
+	
+	public void addRow(Grade grade) {
+		this.rows.add(grade);
 	}
 
 }
