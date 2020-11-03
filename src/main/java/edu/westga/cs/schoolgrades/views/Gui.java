746d330a4d212e211f5ba0ad33d6797103c9a711
Greg Murrell <j_greg_m@comcast.net>
2020-11-03 14:39:12 -0500

    basic gui extension in place

--- src/main/java/edu/westga/cs/schoolgrades/views/Gui.java
+++ src/main/java/edu/westga/cs/schoolgrades/views/Gui.java
@@ -1,5 +1,15 @@
 package edu.westga.cs.schoolgrades.views;
 
+/**
+ * User configuration of the GUI
+ * 
+ * @author Greg Murrell
+ * @version 11/03/2020
+ */
 public class Gui extends GuiWindowBuilderLayout {
-
+	private GuiController controller;
+	
+	public Gui() {
+		this.controller = new GuiController();
+	}
 }
