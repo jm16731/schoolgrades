e4591094d72f21a60cf6eb0c6cbb25662e5745e1
Greg Murrell <j_greg_m@comcast.net>
2020-11-06 16:21:46 -0500

    ready to test

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/CompositeGradeWhenRemoveGrade.java
@@ -0,0 +1,16 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Greg Murrell
+ * @version 11/06/2020
+ */
+class CompositeGradeWhenRemoveGrade {
+
+	
+
+}
