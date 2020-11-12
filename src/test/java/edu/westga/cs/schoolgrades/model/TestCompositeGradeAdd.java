7387b02246b374579a29110124972db33d3538a6
Greg Murrell <j_greg_m@comcast.net>
2020-11-12 14:50:48 -0500

    mock Grade, GradeCalculationStrategy

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAdd.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAdd.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.*;
+
 public class TestCompositeGradeAdd {
 
 	private CompositeGrade composite;
@@ -16,10 +18,10 @@ public class TestCompositeGradeAdd {
 	
 	@BeforeEach
 	public void setup() {
-		composite = new CompositeGrade(new SumOfGradesStrategy());
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
+		composite = new CompositeGrade(mock(GradeCalculationStrategy.class));	
+		grade0 = mock(Grade.class);
+		grade1 = mock(Grade.class);
+		grade2 = mock(Grade.class);
 	}
 	
 	@Test
