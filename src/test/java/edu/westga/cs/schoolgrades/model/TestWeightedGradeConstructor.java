7190a5a1b28653a38416b6d185f9c11f583379d5
Greg Murrell <j_greg_m@comcast.net>
2020-11-12 14:36:06 -0500

    mock SimpleGrade complete

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.*;
+
 public class TestWeightedGradeConstructor {
 
 	@Test
@@ -16,14 +18,14 @@ public class TestWeightedGradeConstructor {
 	@Test
 	public void shouldNotAllowNegativeWeight() {
 		assertThrows(IllegalArgumentException.class, () ->{ 
-			new WeightedGrade(new SimpleGrade(50), -10.0);
+			new WeightedGrade(mock(Grade.class), -10.0);
 		});
 	}
 	
 	@Test
 	public void shouldNotAllowWeightGreaterThanOne() {
 		assertThrows(IllegalArgumentException.class, () ->{ 
-			new WeightedGrade(new SimpleGrade(50), 10.0);
+			new WeightedGrade(mock(Grade.class), 10.0);
 		});
 	}
 }
